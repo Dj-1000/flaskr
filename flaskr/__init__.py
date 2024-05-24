8d83ce2212d666dc9a8ba1ec933ee1daf96b6e63
Dheeraj Sharma <djsharma1000@gmail.com>
2024-05-24 19:07:19 +0530

    added auth views

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,6 +31,9 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    # regustering the auth blueprint
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     
 
